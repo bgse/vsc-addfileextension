9c1f008d20e6933785d6d54960aa817df12bc56f
Nutzer <Nutzer>
2015-12-17 00:00:23 +0100

    Fixes #4, platform path separators

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode'; 
+import * as vscode from 'vscode';
+import * as path from 'path';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -25,13 +26,18 @@ export function activate(context: vscode.ExtensionContext) {
 		// Prompt the user for a path
 		vscode.window.showInputBox({prompt: "Provide file path relative to project folder."}).then(function(psPath: string){
 			
-			// Add leading slash if necessary
-			if(!psPath.startsWith("/")){
-				psPath = "/" + psPath;
+			// Add leading path separator if necessary
+			if(!psPath.startsWith(path.sep)){
+				psPath = path.sep + psPath;
 			}
 			
 			// Add workspace folder to path
 			let lsFullpath: string = vscode.workspace.rootPath + psPath;
+			
+			// Correct path separators to platform default
+			// Allow windows users to use unix-like separators too
+			lsFullpath = path.normalize(lsFullpath.replace(/\//g, path.sep));
+			
 			
 			vscode.workspace.openTextDocument(vscode.Uri.parse("untitled:" + lsFullpath)).then(function(poDocument: vscode.TextDocument){
 				vscode.window.showTextDocument(poDocument).then(function(poEditor: vscode.TextEditor){
